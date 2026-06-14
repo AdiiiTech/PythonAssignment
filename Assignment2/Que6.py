5ce1aa1d076bc13a5bdc16cbd206bbe0e35a8401
Aditya Jadhav <adijadhav1630@gmail.com>
2026-06-15 01:05:43 +0530

    Add files via upload

---
+++ Assignment2/Que6.py
@@ -0,0 +1,15 @@
+print("Enter First Number :" )
+No1 = int(input())
+
+print("Enter Second Number")
+No2 = int(input())
+
+Add= No1 + No2
+Sub = No1 - No2
+Multi = No1 * No2
+Divi = No1 / No2
+
+print("Addition is: ",Add)
+print("subtraction is: ",Sub )
+print("multiplication is: ",Multi )
+print("division is: ",Divi )
