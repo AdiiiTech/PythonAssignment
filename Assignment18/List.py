d4e73ebcdbb608b866539e1a0358f6952e0ba8a1
Aditya Jadhav <adijadhav1630@gmail.com>
2026-07-22 12:42:56 +0530

    Add files via upload

---
+++ Assignment18/List.py
@@ -0,0 +1,18 @@
+from Num import Prime
+
+def main():
+    number = int(input("Enter how many numbers: "))
+    result = []
+
+    for i in range(number):
+        val = int(input("Enter number: "))
+        result.append(val)
+
+    print("Elements of the data:", result)  
+
+    ret = Prime(result)  
+    print("Addition of prime numbers is:", ret)
+
+
+if __name__ == "__main__":
+    main()
